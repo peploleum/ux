965806faeb5ebfcf620e0734851d8ee4939eeb6d
peploleum <peploleum@gmail.com>
2018-02-24 23:08:03 +0100

    main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
