9d1935d472e97bf6c27a5922448f1bbe6f0b17ff
peploleum <peploleum@gmail.com>
2018-02-25 01:52:09 +0100

    main layout v3 with navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavbarItem} from "./navbar-item";
 
 @Component({
   selector: 'navbar',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  navbar: Array<NavbarItem> = [];
+
+  constructor() {
+    let item1: NavbarItem = new NavbarItem('fa fa-globe', 'Awesome nav I');
+    let item2: NavbarItem = new NavbarItem('fa fa-sitemap', 'Awesome nav II');
+    let item3: NavbarItem = new NavbarItem('fa fa-file-o', 'Awesome nav II');
+    let item4: NavbarItem = new NavbarItem('fa fa-plane', 'Awesome nav IV');
+    this.navbar = [item1, item2, item3, item4];
+  }
 
   ngOnInit() {
   }
